067d5429cdcab225448fdd9b69a080b386926c0d
sueraychang <sueraychang@gmail.com>
2020-07-31 18:02:05 +0800

    Request permissions.

--- sample/src/main/java/com/src/webrtc/android/sample/MainActivity.kt
+++ sample/src/main/java/com/src/webrtc/android/sample/MainActivity.kt
@@ -1,48 +1,119 @@
 package com.src.webrtc.android.sample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import com.src.webrtc.android.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate")
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.apply {
+            createRoom.setOnClickListener {
+                Log.d(TAG, "on createRoom click")
+                joinRoom.isEnabled = false
+                if (isPermissionsGranted()) {
+                    viewModel.createRoom()
+                } else {
+                    requestPermissions()
+                }
+            }
+
+            joinRoom.setOnClickListener {
+                Log.d(TAG, "on joinRoom click")
+                createRoom.isEnabled = false
+                if (isPermissionsGranted()) {
+                    viewModel.joinRoom()
+                } else {
+                    requestPermissions()
+                }
+            }
+        }
+
+        // Enable Firestore logging
+        FirebaseFirestore.setLoggingEnabled(true)
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (verifyPermissions(grantResults)) {
+                if (binding.createRoom.isEnabled) {
+                    viewModel.createRoom()
+                } else {
+                    viewModel.joinRoom()
+                }
+            } else {
+                binding.createRoom.isEnabled = true
+                binding.joinRoom.isEnabled = true
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
+    private fun isPermissionsGranted(): Boolean {
+        val result = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+        Log.d(TAG, "isPermissionsGranted: $result")
+        return result
     }
 
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
+    private fun requestPermissions() {
+        Log.d(TAG, "requestPermissions")
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)
+            || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+            val builder = AlertDialog.Builder(this)
+                .setMessage(R.string.connect_permissions_needed)
+            val dialog = builder.create()
+            dialog.show()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                PERMISSIONS,
+                PERMISSION_REQUEST_CODE
+            )
+        }
     }
 
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
+    private fun verifyPermissions(grantResults: IntArray): Boolean {
+        if (grantResults.isEmpty()) {
+            return false
+        }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
+        for (result in grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
     }
 
     companion object {
         private const val TAG = "[wa]MainActivity"
+
+        private const val PERMISSION_REQUEST_CODE = 100
+        private val PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+        )
     }
 }
